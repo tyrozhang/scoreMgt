75f26044468cb85bd3b5900249a3961bdb29541c
tyrozhang <tyroz@qq.com>
2017-09-26 18:57:53 +0800

    教学班DomainTest

---
+++ src/test/scoreMgtTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import domain.share.TeacherPositionEnum;
+import domain.teachingclass.TeachingClass;
+import domain.teachingclass.TeachingClassFactory;
+import infrastructure.entityID.CourseID;
+import infrastructure.entityID.StudentID;
+import infrastructure.entityID.TeacherID;
+import infrastructure.entityID.TeachingClassID;
+import junit.framework.Assert;
+
+public class scoreMgtTest {
+
+	
+	@Test
+	public void testAssignCourseTeachers() {
+		
+		TeachingClassID teachingClassID=new TeachingClassID();
+		CourseID courseID=new CourseID();
+		String courseName="大学英语";
+		TeachingClass aTeachingClass=TeachingClassFactory.createTeachingClassFactory().createTeachingClass(teachingClassID, courseID, courseName);
+		
+		TeacherID assignMajorTeacherID=new TeacherID();
+		aTeachingClass.assignCourseTeacher(assignMajorTeacherID,TeacherPositionEnum.MAJOR);
+		
+		TeacherID assignAssistTeacherID=new TeacherID();
+		aTeachingClass.assignCourseTeacher(assignAssistTeacherID,TeacherPositionEnum.ASSIST);
+		
+		
+		assertEquals(teachingClassID.getID(), aTeachingClass.getTeachingClassID().getID());
+		assertTrue(aTeachingClass.getMajorCourseTeachers().stream().anyMatch(t->t.getID().equals(assignMajorTeacherID.getID())));
+	}
+
+	@Test
+	public void testAssignStudent() {
+		
+		TeachingClassID teachingClassID=new TeachingClassID();
+		CourseID courseID=new CourseID();
+		String courseName="大学物理";
+		TeachingClass aTeachingClass=TeachingClassFactory.createTeachingClassFactory().createTeachingClass(teachingClassID, courseID, courseName);
+		
+		StudentID firstStudentID=new StudentID();
+		aTeachingClass.assignStudent(firstStudentID);
+		
+		StudentID secondStudentID=new StudentID();
+		aTeachingClass.assignStudent(secondStudentID);
+		
+		assertTrue(aTeachingClass.getStudents().stream().anyMatch(t->t.getID().equals(firstStudentID.getID())));
+		assertTrue(aTeachingClass.getStudents().stream().anyMatch(t->t.getID().equals(secondStudentID.getID())));
+		assertFalse(aTeachingClass.getStudents().stream().anyMatch(t->t.getID().equals("notExistStudentID")));
+	}
+}
