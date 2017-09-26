f6a68077c216100e8a0b1eb4e070af23697871eb
tyrozhang <tyroz@qq.com>
2017-09-26 18:44:32 +0800

    教学任务组件领域代码实现

---
+++ src/infrastructure/entityID/CourseID.java
@@ -0,0 +1,10 @@
+package infrastructure.entityID;
+
+public class CourseID extends EntityID {
+
+	public CourseID() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/infrastructure/entityID/TeachingClassID.java
@@ -0,0 +1,13 @@
+package infrastructure.entityID;
+
+import com.sun.xml.internal.stream.Entity;
+
+public class TeachingClassID extends EntityID {
+
+	public TeachingClassID() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
